bd062ab3971204c3265b7b24c92afe69a7822239
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2022-08-21 01:31:19 -0300

    [Feature] Made internals visible to other proyects

---
+++ src/MamisSolidarias.Infrastructure.TEMPLATE/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("MamisSolidarias.WebAPI.TEMPLATE")]
+[assembly: InternalsVisibleTo("MamisSolidarias.WebAPI.TEMPLATE.Test")]
+[assembly: InternalsVisibleTo("MamisSolidarias.HttpClient.TEMPLATE.Test")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
