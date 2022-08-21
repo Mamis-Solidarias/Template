7bb1d737f522bb6fd2285a0af9d78c8663ff5709
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2022-08-21 01:27:33 -0300

    [Feature] Removed ReadyRequest

--- src/MamisSolidarias.HttpClient.TEMPLATE/TEMPLATEClient/TEMPLATEClient.GetTest.cs
+++ src/MamisSolidarias.HttpClient.TEMPLATE/TEMPLATEClient/TEMPLATEClient.GetTest.cs
@@ -4,10 +4,10 @@ namespace MamisSolidarias.HttpClient.TEMPLATE.TEMPLATEClient;
 
 public partial class TEMPLATEClient
 {
-    public async Task<Response?> GetTestAsync(Request requestParameters, CancellationToken token = default)
+    public Task<Response?> GetTestAsync(Request requestParameters, CancellationToken token = default)
     {
-        return await CreateRequest<Response>(HttpMethod.Get, "user", requestParameters.Name)
-            .ExecuteAsync(token);
+        return CreateRequest(HttpMethod.Get, "user", requestParameters.Name)
+            .ExecuteAsync<Response>(token);
 
     }
 }
