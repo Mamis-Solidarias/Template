048b414bac6d9faafd6c1ecc53addcc523185b58
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2022-10-07 13:02:26 -0300

    [Refactor] Improved ServiceCollection extensions

--- src/MamisSolidarias.HttpClient.TEMPLATE/ServiceCollectionExtensions.cs
+++ src/MamisSolidarias.HttpClient.TEMPLATE/ServiceCollectionExtensions.cs
@@ -1,6 +1,8 @@
 using MamisSolidarias.HttpClient.TEMPLATE.Models;
+using MamisSolidarias.HttpClient.TEMPLATE.Services;
 using MamisSolidarias.HttpClient.TEMPLATE.TEMPLATEClient;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Polly;
@@ -12,24 +14,34 @@ public static class ServiceCollectionExtensions
     /// <summary>
     /// It registers the TEMPLATEHttpClient using dependency injection
     /// </summary>
-    /// <param name="builder"></param>
-    public static void AddTEMPLATEHttpClient(this WebApplicationBuilder builder)
+    /// <param name="services"></param>
+    /// <param name="configuration"></param>
+    public static void AddTEMPLATEHttpClient(this IServiceCollection services, IConfiguration configuration)
     {
-        var configuration = new TEMPLATEConfiguration();
-        builder.Configuration.GetSection("TEMPLATEHttpClient").Bind(configuration);
-        ArgumentNullException.ThrowIfNull(configuration.BaseUrl);
-        ArgumentNullException.ThrowIfNull(configuration.Timeout);
-        ArgumentNullException.ThrowIfNull(configuration.Retries);
+        var config = new TEMPLATEConfiguration();
+        configuration.GetSection("TEMPLATEHttpClient").Bind(config);
+        ArgumentNullException.ThrowIfNull(config.BaseUrl);
+        ArgumentNullException.ThrowIfNull(config.Timeout);
+        ArgumentNullException.ThrowIfNull(config.Retries);
 
-        builder.Services.AddSingleton<ITEMPLATEClient, TEMPLATEClient.TEMPLATEClient>();
-        builder.Services.AddHttpClient("TEMPLATE", client =>
+        services.AddHttpContextAccessor();
+        services.AddSingleton<ITEMPLATEClient, TEMPLATEClient.TEMPLATEClient>();
+        services.AddHttpClient("TEMPLATE", (services,client) =>
         {
-            client.BaseAddress = new Uri(configuration.BaseUrl);
-            client.Timeout = TimeSpan.FromMilliseconds(configuration.Timeout);
-            client.DefaultRequestHeaders.Add("Content-Type", "application/json");
+            client.BaseAddress = new Uri(config.BaseUrl);
+            client.Timeout = TimeSpan.FromMilliseconds(config.Timeout);
+            
+            var contextAccessor = services.GetService<IHttpContextAccessor>();
+            if (contextAccessor is not null)
+            {
+                var authHeader = new HeaderService(contextAccessor).GetAuthorization();
+                if (authHeader is not null)
+                    client.DefaultRequestHeaders.Add("Authorization", authHeader);
+            }
+            
         })
             .AddTransientHttpErrorPolicy(t =>
-            t.WaitAndRetryAsync(configuration.Retries,
+            t.WaitAndRetryAsync(config.Retries,
                 retryAttempt => TimeSpan.FromMilliseconds(100 * Math.Pow(2, retryAttempt)))
         );
     }
